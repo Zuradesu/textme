5660b68bdbab96035fbe0a25e767784fa1d018f1
A-Aldo <aldo_irianto_30rpl@student.smktelkom-mlg.sch.id>
2025-05-09 14:47:38 +0700

    Remove .env.local from Git tracking

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMiCrLClPRJZhwlLGwMs7eKPBVaLJ-liU",
-  authDomain: "askme-49fb7.firebaseapp.com",
-  projectId: "askme-49fb7",
-  storageBucket: "askme-49fb7.firebasestorage.app",
-  messagingSenderId: "371357700059",
-  appId: "1:371357700059:web:9b7782eecc79260cd121fe",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
